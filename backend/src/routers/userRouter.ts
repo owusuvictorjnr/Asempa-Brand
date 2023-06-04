ffa5a405f68dad313a3338888c5a05e577a6b2be
owusuvictorjnr <owusuvictor80@gmail.com>
2023-06-04 15:46:22 -0400

    Create update Auth

--- backend/src/routers/userRouter.ts
+++ backend/src/routers/userRouter.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express';
-import asyncHandler from 'express-async-handler';
-import bcrypt from 'bcryptjs';
-import { generateToken } from '../utils';
-import { User, UserModel } from '../models/userModel';
+import express, { Request, Response } from 'express'
+import asyncHandler from 'express-async-handler'
+import bcrypt from 'bcryptjs'
+import { generateToken, isAuth } from '../utils'
+import { User, UserModel } from '../models/userModel'
 
-export const userRouter = express.Router();
+export const userRouter = express.Router()
 userRouter.post(
   '/signin',
   asyncHandler(async (req: Request, res: Response) => {
-    const user = await UserModel.findOne({ email: req.body.email });
+    const user = await UserModel.findOne({ email: req.body.email })
     if (user) {
       if (bcrypt.compareSync(req.body.password, user.password)) {
         res.json({
@@ -17,13 +17,13 @@ userRouter.post(
           email: user.email,
           isAdmin: user.isAdmin,
           token: generateToken(user),
-        });
-        return;
+        })
+        return
       }
     }
-    res.status(401).json({ message: 'Invalid email or password' });
+    res.status(401).json({ message: 'Invalid email or password' })
   })
-);
+)
 
 userRouter.post(
   '/signup',
@@ -32,13 +32,38 @@ userRouter.post(
       name: req.body.name,
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password),
-    } as User);
+    } as User)
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
       token: generateToken(user),
-    });
+    })
   })
-);
+)
+
+userRouter.put(
+  '/profile',
+  isAuth,
+  asyncHandler(async (req: Request, res: Response) => {
+    const user = await UserModel.findById(req.user._id)
+    if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8)
+      }
+      const updateUser = await user.save()
+      res.send({
+        _id: updateUser._id,
+        name: updateUser.name,
+        email: updateUser.email,
+        isAdmin: updateUser.isAdmin,
+        token: generateToken(updateUser),
+      })
+      return
+    }
+    res.status(404).json({ message: 'User not found' })
+  })
+)
