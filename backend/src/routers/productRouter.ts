38c63b65e7ce6e91401d328ca3802f774fcd3b7f
owusuvictorjnr <owusuvictor80@gmail.com>
2023-06-04 18:24:45 -0400

    create categories api

--- backend/src/routers/productRouter.ts
+++ backend/src/routers/productRouter.ts
@@ -1,25 +1,33 @@
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import { ProductModel } from '../models/productModel';
+import express, { Request, Response } from 'express'
+import asyncHandler from 'express-async-handler'
+import { ProductModel } from '../models/productModel'
 
-export const productRouter = express.Router();
+export const productRouter = express.Router()
 
 productRouter.get(
   '/',
   asyncHandler(async (req, res) => {
-    const products = await ProductModel.find();
-    res.json(products);
+    const products = await ProductModel.find()
+    res.json(products)
   })
-);
+)
+
+productRouter.get(
+  '/categories',
+  asyncHandler(async (req: Request, res: Response) => {
+    const categories = await ProductModel.find().distinct('category')
+    res.json(categories)
+  })
+)
 
 productRouter.get(
   '/slug/:slug',
   asyncHandler(async (req, res) => {
-    const product = await ProductModel.findOne({ slug: req.params.slug });
+    const product = await ProductModel.findOne({ slug: req.params.slug })
     if (product) {
-      res.json(product);
+      res.json(product)
     } else {
-      res.status(404).json({ messsage: 'Product Not Found' });
+      res.status(404).json({ messsage: 'Product Not Found' })
     }
   })
-);
+)
